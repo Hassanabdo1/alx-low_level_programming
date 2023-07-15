1ca5c8250c28bd858bf7a89381ac919a0ccd7f14
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-15 07:36:10 -0700

    my first line C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/**
+ * main - entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Alawys 0 (success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9;)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
