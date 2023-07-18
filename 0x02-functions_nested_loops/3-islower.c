33a6dbf1b98ea877bdefe41ef2fc806094c50233
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-18 09:59:05 -0700

    my first line C

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,13 @@
 *	character is lowercase
 *
 * @c: checks input of funktion
-* return: return 1 if 'c' is lowercase
+*
+* Return: returns 1 if `c` is lowercase
 * otherwise always 0 (success)
 */
-/* Returns 1 if c is lowercase, 0 otherwise. */
+
 int _islower(int c)
-{	/* Check if c is between 'a' and 'z', inclusive. */
+{	
 	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
