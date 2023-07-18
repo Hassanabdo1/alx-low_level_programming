f5b019772aac5256a14319632954e108305f6778
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-18 08:50:20 -0700

    my first line C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * prints the alphabet - utlilzes on the _putchar function toprint
+ * the alohabet a - z
+ */
+
+void print_alphabet(void)
+}
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+	_putchar('\n');
+}
