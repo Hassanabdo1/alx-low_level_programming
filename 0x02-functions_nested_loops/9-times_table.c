e8e3a2df014e1b31980b8667b2a6e91c680572a8
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-18 16:37:48 -0700

    my first line C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 			 * * put space if profuct is a single number
 			 * place the first digit if its two numbers
 			 * */
- 			if (pord <= 9)_putchar(' ');
+ 			if (prod <= 9)_putchar(' ');
 		       else
 				_putchar((prod / 10) + 48 ); /*get the first digit*/
 			_putchar((prod % 10) + 48); /*get the second digit*/
