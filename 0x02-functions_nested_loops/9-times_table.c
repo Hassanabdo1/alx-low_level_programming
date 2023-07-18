b39fea2883bd837d82bd5b9600eccb1dfc5453d7
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-18 16:43:11 -0700

    my first line C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-	       for (mult = 1; mult <= 9; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -26,12 +26,13 @@ void times_table(void)
 			/**
 			 * * put space if profuct is a single number
 			 * place the first digit if its two numbers
-			 * */
- 			if (prod <= 9)_putchar(' ');
-		       else
+			 */
+			if (prod <= 9)
+				_putchar(' ');
+			else
 				_putchar((prod / 10) + 48 ); /*get the first digit*/
 			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
-		_putchar('\n');  
+		_putchar('\n');
 	}
 }	
