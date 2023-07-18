efd103d8b6104222147a0643fc1cd8a10c540d5c
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-18 08:15:02 -0700

    my first line C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,14 @@
  * Return: Always 0 (success)
  */
 
-int main (void)
-
+int main(void)
+{
 	char str[] = "_putchar";
 	int ch;
-	for (ch =0; ch < 8 ; ch++)
-			_putchar(str[ch]);
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
 	_putchar('\n');
 
 	return (0);
+}
