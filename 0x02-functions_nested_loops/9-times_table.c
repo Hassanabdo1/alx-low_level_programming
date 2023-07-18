5746d752fb7348389a5e985e156da70c91d08b5d
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-18 16:44:28 -0700

    my first line C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,9 +30,9 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 48 ); /*get the first digit*/
+				_putchar((prod / 10) + 48); /*get the first digit*/
 			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
 		_putchar('\n');
 	}
-}	
+}
