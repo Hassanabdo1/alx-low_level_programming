5357419cf1f0478af51bf14e830ba7a51e8fe186
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-18 16:33:59 -0700

    my first line C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,10 +13,10 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num =< 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		-putchar(48);
-	       for (mult = 1; mult < 9; mult++)
+	       for (mult = 1; mult <= 9; mult++)
 		{
 			-putchar(',');
 			-putchar(' ');
