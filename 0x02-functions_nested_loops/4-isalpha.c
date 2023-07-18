25d1eeab09e1f12dc0a46967f0d9b4e46a01b1a2
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-18 13:53:20 -0700

    my first line C

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,11 +6,11 @@
  *
  * @c: takes input from other function.
  *
- * Return 1 is c if true else 0
+ * Return: 1 is c if true else 0
  */
 
 int _isalpha(int c)
-{/* Returns 1 if c is lowercase, 0 otherwise. */
+{
 	if (c >= 97 && c <= 122)
 		return (1);
 	if (c >= 65 && c <= 90)
