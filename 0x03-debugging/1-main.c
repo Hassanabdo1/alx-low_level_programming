3b91a097dc917fb5426ab40da1a70aa694931237
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-19 06:11:33 -0700

    my first line C

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
 
-void positive_or_negative(int i);
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/* while ( i < 10 )
+	 * *{
+	 * 	putchar(i);
+	 * }*/
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
