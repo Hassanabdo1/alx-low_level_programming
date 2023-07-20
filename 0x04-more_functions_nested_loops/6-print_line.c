6d16e757de01982677a75c3a4d73c7245825089b
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-20 16:58:55 -0700

    my first line C

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ * 
+ * @n: is the number of times the _ character
+ * should be printed
+ */
+
+void print_line(int n)
+{
+        int inchr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (inchr = 1; inchr <= n; inchr++)
+			_putchar('_');
+		_putchar('\n');
+	}
+
+}
