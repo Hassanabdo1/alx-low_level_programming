0b8bda746e08ca7fbec38e3433a8ef543734c969
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-20 14:02:03 -0700

    my first line C

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *
+ * inle using _putchar twice
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	}while (num >= 0 && num <= 9);
+	_putchar('\n');	
+
+}
