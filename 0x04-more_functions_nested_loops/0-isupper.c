2efeed18ae197ed4ad3aca96812a51fa77105473
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-20 13:27:34 -0700

    my first line C

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _isupper -check if c is upper
+ * _isupper - check if c is upper
  *
  * @c: input for alphabet
  *
- * Return: 1 if its uppercase. 0 if not
+ * Return: 1 if its uppercase, 0 if not
  */
 
 int _isupper(int c)
