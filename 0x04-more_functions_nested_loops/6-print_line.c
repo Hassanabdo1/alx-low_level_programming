6d6f957a1c88b0634e50f14256666169a6285712
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-20 17:32:32 -0700

    my first line C

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,6 +2,7 @@
 
 /**
  * print_line - print a straight line
+ *
  * @n: is the number of times the _ character
  * should be printed
  */
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,7 +10,5 @@ void print_numbers(void);
 void print_most_numbers(void;
 void print_line(int n);
 void more_numbers(void);
-void print_line(int n);
-int _putchar(char c);
 
 #endif
