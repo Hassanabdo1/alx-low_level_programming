52464d42ee21fadae9dba5382a8fc0a85990c995
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-20 17:02:20 -0700

    my first line C

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,13 @@
 
 /**
  * print_line - print a straight line
- * 
  * @n: is the number of times the _ character
  * should be printed
  */
 
 void print_line(int n)
 {
-        int inchr;
+	int inchr;
 
 	if (n <= 0)
 		_putchar('\n');
