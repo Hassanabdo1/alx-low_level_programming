d5c297ac6e01cdc2f907249cc883737031e36960
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-21 02:42:00 -0700

    my first line C

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - print 0 - 14 ten times and you
- * can onlu use _putchar five times
- *
- * Return: Always 0 (success)
+ * more_numbers - print 0 - 14 ten times using _putchar three times only
  */
-
 void more_numbers(void)
 {
 	int num, row, count;
@@ -15,13 +11,13 @@ void more_numbers(void)
 	{
 		for (count = 0; count <= 14; count++)
 		{
-			num = count;
+			num = count
 			if (count > 9)
 			{
-				_putchar(1 + 48);
-				num = count % 10;
+				_putchar((count / 10) + '0'); // Print the tens digit
+				num = count % 10; // Get the ones digi
 			}
-			_putchar(num + 48);
+			_putchar(num + '0'); // Print the ones digit
 		}
 		_putchar('\n');
 	}
