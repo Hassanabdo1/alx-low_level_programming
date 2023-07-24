facb202b782d185dd5cf9d6683116954ee67183d
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-07-24 10:55:56 -0700

    my first line C

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * puts_array - prints n elements of an array of integers
+ * print_array - prints n elements of an array of integers
  * @a: Array of integers
- * @n: Number of elements of the array to be printed 
+ * @n: Number of elements of the array to be printed
  * Return: void
  */
 
