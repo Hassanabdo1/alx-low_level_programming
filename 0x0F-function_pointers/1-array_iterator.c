8a1ceb5d2380361eae2868891c99ba435efaee34
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-08-17 01:51:00 +0300

    my first line C

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 /**
  * array_iterator - prints each array elem on a newl
- * @array: array 
+ * @array: array
  * @size: how many elem to print
  * @action: pointer to print in regular or hex
- * Return: void 
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
