410c10824efb33aa979ae5a98ad4d3cb30260737
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-08-17 00:50:43 +0300

    my first line C

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - prints the each for array elem on a newl
+ * @array: array 
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return: void 
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (j = 0; j < size; j++)
+	{
+		action(array[j]);
+	}
+}
