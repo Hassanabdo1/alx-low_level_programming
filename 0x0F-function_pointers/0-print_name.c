d60f3b8f3b855dfca53693b3d617990c7efe6f21
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-08-17 00:36:37 +0300

    my first line C

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - we wont to print the name using pointer with function
+ * print_name - print name using pointer to function
  * @name: string to add
  * @f: pointer to function
  * Return: nathing
