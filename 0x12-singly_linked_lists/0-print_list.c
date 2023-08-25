c15c07b79c2ec34b590ec29e4a36d2f7c9128bdc
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-08-25 05:42:42 +0300

    by hassan abdo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,19 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
- *
- * Return: the number of nodes printed
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * brief Prints the contents of a linked list.
+ * Return: number of elements in the list.
+ * 
+ * If a string is NULL, it prints "(nil)" instead.
+ * * Return: number of elements in the list.
+ */
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
 size_t print_list(const list_t *h)
 {
