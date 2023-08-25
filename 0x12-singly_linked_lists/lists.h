29e3862dcad9e7fe84c8f0aecd1ba3baa2727861
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-08-25 05:32:30 +0300

    by hassan abdo

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,55 +6,24 @@
 #include <string.h>
 
 /**
- * @file lists.h
- * @brief This file contains the definition of a linked list structure and related functions for string manipulation.
- */
-
-/**
- * @struct list_s
- * @brief Structure representing a node in the linked list.
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-    char *str;               /**< Pointer to the string data */
-    unsigned int len;        /**< Length of the string */
-    struct list_s *next;     /**< Pointer to the next node */
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-/**
- * @brief Prints the strings stored in the linked list.
- * @param h Pointer to the head of the list.
- * @return The number of nodes in the list.
- */
 size_t print_list(const list_t *h);
-
-/**
- * @brief Counts the number of nodes in the linked list.
- * @param h Pointer to the head of the list.
- * @return The number of nodes in the list.
- */
 size_t list_len(const list_t *h);
-
-/**
- * @brief Adds a new node with the given string to the beginning of the list.
- * @param head Pointer to a pointer to the head of the list.
- * @param str String to be stored in the new node.
- * @return Pointer to the new head of the list.
- */
 list_t *add_node(list_t **head, const char *str);
-
-/**
- * @brief Adds a new node with the given string to the end of the list.
- * @param head Pointer to a pointer to the head of the list.
- * @param str String to be stored in the new node.
- * @return Pointer to the head of the list.
- */
 list_t *add_node_end(list_t **head, const char *str);
-
-/**
- * @brief Frees the memory used by the linked list.
- * @param head Pointer to the head of the list.
- */
 void free_list(list_t *head);
 
 #endif
