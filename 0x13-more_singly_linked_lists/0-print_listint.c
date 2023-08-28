614d1a99a1952c9d69934605d56c5ee801a800df
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-08-28 23:57:35 +0300

    my first line C

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
- */
+ * print_listint - prints a linked lists
+ * @h: pointer to first node
+ * Return: size of list
+*/
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
+    size_t i = 0;
 
     while (h)
     {
-    printf("%d\n", h->n);
-    num++;
-    h = h->next;
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
     }
-
-    return (num);
+    return (i);
 }
