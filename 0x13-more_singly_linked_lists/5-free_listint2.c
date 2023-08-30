ba15479cb85b40c1b8548bfd7a66ddcabfa39a3e
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-08-31 00:10:02 +0300

    my first line C

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,23 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * free_listint2 - frees a list of ints
+ * @head: address of pointer to first node
+ *
+ * Return: void 
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *nod;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		temp = (*head)->next;
+		nod = (*head)->next;
 		free(*head);
-		*head = temp;
+		*head = nod;
 	}
 
 	*head = NULL;
