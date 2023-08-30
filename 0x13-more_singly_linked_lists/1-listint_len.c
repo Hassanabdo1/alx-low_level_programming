eeaefb725bc0bae699405fa2fbac448bf54babf4
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-08-30 20:44:06 +0300

    my first line C

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,20 @@
-#include "stdio.h"
+#include "lists.h"
 
-int main(){
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
 
-    printf("hello wolrd");
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
 }
