3a9f766e25c3198e0634585001f25fbae1c4425b
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-08-30 23:51:49 +0300

    my first line C

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,17 +4,17 @@
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
  *
- * Return: number of nodes
+ * Return: size of list
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	size_t j = 0;
 
 	while (h)
 	{
-		num++;
+		j++;
 		h = h->next;
 	}
 
-	return (num);
+	return (j);
 }
