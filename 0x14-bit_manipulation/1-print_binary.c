7dbc44101a18fedab0a02801d8fc5672fbca47f5
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-09-02 00:30:43 +0300

    my first line C

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,25 +2,27 @@
 
 /**
  * print_binary - prints the binary equivalent of a decimal number
- * @n: number to print in binary
+ * @n: the number to print in binary
+ * 
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
+	int nop, counts = 0;
 	unsigned long int current;
 
-	for (i = 63; i >= 0; i--)
+	for (nop = 63; nop >= 0; nop--)
 	{
-		current = n >> i;
+		current = n >> nop;
 
 		if (current & 1)
 		{
 			_putchar('1');
-			count++;
+			counts++;
 		}
-		else if (count)
+		else if (counts)
 			_putchar('0');
 	}
-	if (!count)
+	if (!counts)
 		_putchar('0');
 }
