5b3ac596501820f5aaacf23369616a03702b3220
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-09-02 00:42:43 +0300

    my first line C

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * get_endianness - checks if a machine is little or big endian
- * Return: 0 for big, 1 for little
+ * get_endianness - returns the endianness of the system
+ * Return: 0 for big, 1 for small
  */
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (char *) &i;
+	unsigned int j = 1;
+	char *c = (char *) &j;
 
 	return (*c);
 }
