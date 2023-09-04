d7f32eef8f50b09fa6e026b328b81a4d0a2490aa
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-09-05 00:49:10 +0300

    my first line C

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,26 +3,29 @@
 
 /**
  * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * @filename: name of file to read
+ * @letters: number of bytef to read
+ *
+ * Return: number bytes read/printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	ssize_t bytes;
+	ssize_t num;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	num = read(fd, buf, letters);
+	bytes = bytesrite(STDOUT_FILENO, buf, num);
 
 	free(buf);
 	close(fd);
-	return (w);
+	return (bytes);
+
+
+        
 }
