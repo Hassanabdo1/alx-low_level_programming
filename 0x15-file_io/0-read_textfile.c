36aeabe58b2a1fcb25d88495fb53bb84bea7e19c
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-09-05 01:06:59 +0300

    my first line C

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,26 +3,26 @@
 
 /**
  * read_textfile- Read text file print to STDOUT.
- * @filename: name of file to read
- * @letters: number of bytef to read
- *
- * Return: number bytes read/printed
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes;
-    char buf[READ_BUF_SIZE * 8];
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    if (!filename || !letters)
-            return (0);
-    fd = open(filename, O_RDONLY);
-        if (fd == -1)
-            return (0);
-    bytes = read(fd, &buf[0], letters);
-    bytes = write(STDOUT_FILENO, &buf[0], bytes);
-    close(fd);
-    return (bytes);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write("STDOUT_FILENO", buf, t);
 
-        
+	free(buf);
+	close(fd);
+	return (w);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+
 #define READ_BUF_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
