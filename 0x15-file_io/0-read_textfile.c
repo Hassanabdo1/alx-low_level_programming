cc546dae7607efbc1df58e010e0ad57ba444b26f
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-09-05 01:35:25 +0300

    my first line C

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,22 +10,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	{
-	int i;
-	unsigned int dec_val = 0;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (!b)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
-
-	return (dec_val);
-}
+	free(buf);
+	close(fd);
+	return (w);
 }
 
 
