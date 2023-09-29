276a0c3a0b198420318577416383fa8325223aae
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-09-29 04:00:05 +0300

    my first line C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,9 +22,9 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' || b[i] > '1')
+		if (b[i] != '0' && b[i] > '1')
 			return (0);
-		result = result * 2 + (b[i] - '0');
+		result = 2 * result + (b[i] - '0');
 	}
 
 	return (result);
