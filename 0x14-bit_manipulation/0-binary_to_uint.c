9005d7da9a376ac2519329d185572eaf875852dc
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-09-29 04:04:30 +0300

    my first line C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int tmp = 0;
+	unsigned int nop = 0;
 
 	if (!b)
 		return (0);
@@ -24,8 +24,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		tmp = 2 * tmp + (b[i] - '0');
+		nop = 2 * nop + (b[i] - '0');
 	}
 
-	return (tmp);
+	return (nop);
 }
