05d4ec937676aa46fc213414deda2de651c6a7bc
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-09-29 04:20:55 +0300

    my first line C

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,28 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at an index in a decimal number
- * @n: number to index
- * @index: the bit to get
+ * get_bit - Retrieves the value of a specific bit at a given index in a number.
+ * @n: The decimal number to extract the bit from.
+ * @index: The index of the bit to retrieve, where 0 is the least significant bit.
  *
- * Return: the bit state or -1 on error
+ * This function returns the state (0 or 1) of the bit at the specified index in
+ * the binary representation of the number 'n'.
+ *
+ * @n: The decimal number to extract the bit from.
+ * @index: The index of the bit to retrieve.
+ *
+ * Return: The bit value (0 or 1) at the specified index, or -1 if 'index' is
+ *         out of range (greater than 63 for a 64-bit number).
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int nobe;
+	int bit;
 
 	if (index > 63)
 		return (-1);
 
-	nobe = (n >> index) & 1;
+	bit = (n >> index) & 1;
 
-	return (nobe);
+	return (bit);
 }
+
