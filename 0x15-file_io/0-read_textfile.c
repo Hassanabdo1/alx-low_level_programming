98057fd5da6ca92df659726e3af6c07f6ae15951
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-10-03 00:56:55 +0300

    my first line C

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,27 +2,25 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
+ * read_textfile - Reads text file from a file and prints it
+ * @filename: name of file to read
+ * @letters: number of bytes to read
  *
- * Return: w- actual number of bytes read and printed
+ * Return: number bytes read/printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t byest;
-	ssize_t tmp;
-	ssize_t tfil;
+	int tmp;
+    ssize_t bytes;
+    char buf[READ_BUF_SIZE * 8];
 
-	byest = open(filename, O_RDONLY);
-	if (byest == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	tfil = read(byest, buf, letters);
-	tmp = write(STDOUT_FILENO, buf, tfil);
-
-	free(buf);
-	close(byest);
-	return (tmp);
+    if (!filename || !letters)
+            return (0);
+    tmp = open(filename, O_RDONLY);
+    if (tmp == -1)
+            return (0);
+    bytes = read(tmp, &buf[0], letters);
+    bytes = write(STDOUT_FILENO, &buf[0], bytes);
+    close(tmp);
+    return (bytes);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-
+#include <unistd.h>
 
 #define READ_BUF_SIZE 1024
 
