45414a8b3d756cb630fa7eb6bee9b0a88a8e6fb4
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-10-03 00:40:07 +0300

    my first line C

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,10 +14,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
     char buf[READ_BUF_SIZE * 8];
 
     if (!filename || !letters)
-        return (0);
+            return (0);
     tmp = open(filename, O_RDONLY);
     if (tmp == -1)
-        return (0);
+            return (0);
     bytes = read(tmp, &buf[0], letters);
     bytes = write(STDOUT_FILENO, &buf[0], bytes);
     close(tmp);
