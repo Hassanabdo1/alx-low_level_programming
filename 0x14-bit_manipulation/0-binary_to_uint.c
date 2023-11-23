13c57211659824461f7c3668df342fc9be832a0c
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-11-24 01:44:31 +0200

    update 4

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,18 +13,14 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-
-	// Iterate through the string until the end
+		
 	while (b[i])
 	{
-		// Check if the character is not a binary digit (0 or 1)
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
 
-		// Update the result by shifting left and adding the current digit
 		result = (result << 1) + (b[i] - '0');
 
-		// Move to the next character in the string
 		i++;
 	}
 
