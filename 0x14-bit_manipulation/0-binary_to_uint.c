bc8130db2aa9dad88a988858a29204001f5e9012
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-11-24 01:43:24 +0200

    update 4

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,27 +2,27 @@
 
 /**
  * binary_to_uint - Convert a binary number to an unsigned int.
- * @c: The binary number as a string.
+ * @b: The binary number as a string.
  *
  * Return: The unsigned int form of the number.
  */
-unsigned int binary_to_uint(const char *c)
+unsigned int binary_to_uint(const char *b)
 {
 	int i = 0;
 	unsigned int result = 0;
 
-	if (!c)
+	if (!b)
 		return (0);
 
 	// Iterate through the string until the end
-	while (c[i])
+	while (b[i])
 	{
 		// Check if the character is not a binary digit (0 or 1)
-		if (c[i] < '0' || c[i] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 
 		// Update the result by shifting left and adding the current digit
-		result = (result << 1) + (c[i] - '0');
+		result = (result << 1) + (b[i] - '0');
 
 		// Move to the next character in the string
 		i++;
