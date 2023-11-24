ce3ec0705cb29205d1c1fd91efe0532c9f8cfc96
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-11-24 02:13:15 +0200

    update 4

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,6 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
-		return (-1);
+    return (index > 63) ? -1 : (*n |= (1UL << index), 1);
 
-	*n = ((1UL << index) | *n);
-	return (1);
 }
