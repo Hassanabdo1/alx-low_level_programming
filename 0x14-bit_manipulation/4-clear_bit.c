33e3fd9fd8f1e9d3d786bd1c842ebd21a5ca2a70
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-11-24 02:23:14 +0200

    update 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * clear_bit - Clears (sets to 0) the bit at a specified index.
+ * clear_bit - Clear the bit at a specified index.
  * @n: Pointer to the number to modify.
- * @index: The index of the bit to clear.
+ * @index: Index of the bit to clear.
  *
- * Return: 1 on success, -1 on failure (if 'index' is out of range).
+ * Return: 1 on success, -1 on error (invalid index).
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long *n, unsigned int index)
 {
 	if (index > 63)
-		return (-1);
 
-	*n = (~(1UL << index) & *n);
-	return (1);
+		return -1;
+
+	*n &= ~(1UL << index);
+
+	return 1;
 }
