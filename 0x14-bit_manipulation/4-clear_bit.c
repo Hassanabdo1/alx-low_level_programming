593202378fbe4c1c7fc260440730c55da5c325fe
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-11-24 02:25:02 +0200

    update 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,13 +7,11 @@
  *
  * Return: 1 on success, -1 on error (invalid index).
  */
-int clear_bit(unsigned long *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
+		return (-1);
 
-		return -1;
-
-	*n &= ~(1UL << index);
-
-	return 1;
+	*n = (~(1UL << index) & *n);
+	return (1);
 }
