4d279087e51d4d6d836ee8b7a61e3ae33e638b6e
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-11-24 02:07:46 +0200

    update 4

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,8 +9,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int nah;
+
 	if (index > 63)
 		return (-1);
 
-	return (n >> index) & 1;
+	nah = (n >> index) & 1;
+
+	return (nah);
 }
