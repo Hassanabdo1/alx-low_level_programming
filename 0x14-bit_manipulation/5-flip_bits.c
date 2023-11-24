ed811159a176c2a1400eecfe6bc494d0659f0bdb
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-11-24 02:28:15 +0200

    update 4

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
 /**
- * flip_bits - Counts the number of differing bits between two numbers.
+ * flip_bits - Count differing bits between two numbers.
  * @n: The first number.
  * @m: The second number.
  *
- * This function calculates the number of bits that need to be flipped in order
- * to convert 'n' into 'm'.
- *
- * Return: The count of differing bits.
+ * Returns the count of differing bits between 'n' and 'm'.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, count = 0;
-	unsigned long int nope;
-	unsigned long int nums = n ^ m;
+	int j, cont = 0;
+	unsigned long int tmp;
+	unsigned long int numbe = n ^ m;
 
-	for (i = 63; i >= 0; i--)
+	for (j = 63; j >= 0; j--)
 	{
-		nope = nums >> i;
-		if (nope & 1)
-			count++;
+		tmp = numbe >> j;
+		if (tmp & 1)
+			cont++;
 	}
 
-	return (count);
+	return (cont);
 }
