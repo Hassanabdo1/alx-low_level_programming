c9953ee1c65e59cda0105a251cc8079bf4ae5316
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-11-28 03:50:56 +0200

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -53,7 +53,7 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
+	int in, out, rimnder, writ;
 	char *buffer;
 
 	if (argc != 3)
@@ -63,21 +63,21 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	in = open(argv[1], O_RDONLY);
+	rimnder = read(in, buffer, 1024);
+	out = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || r == -1)
+		if (in == -1 || rimnder == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
+				"Error: Can't read in file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		writ = write(out, buffer, rimnder);
+		if (out == -1 || writ == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
@@ -85,14 +85,14 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+		rimnder = read(in, buffer, 1024);
+		out = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (r > 0);
+	} while (rimnder > 0);
 
 	free(buffer);
-	close_file(from);
-	close_file(to);
+	close_file(in);
+	close_file(out);
 
 	return (0);
 }
